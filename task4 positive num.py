62fa0eb642ae53cef4ebfe9eaa2420358efdae13
garv1189 <124590467+garv1189@users.noreply.github.com>
2023-02-18 17:54:18 +0530

    Add files via upload

---
+++ task4 positive num.py
@@ -0,0 +1,11 @@
+# Define the list of numbers
+numbers1 = [12,-7,5,64,-14]
+numbers2 =[12,14,-95,3]
+# Iterate over the list of numbers and print positive numbers
+for num1 in numbers1:
+    if num1 > 0:
+        print(num1)
+        
+for num2 in numbers2: 
+    if num2>0:
+        print(num2)       
